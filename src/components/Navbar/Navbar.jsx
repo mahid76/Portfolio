13c63106adcb6218e4f247498251670a2ddb2e5b
Poblaaa <mahidulislamsohan76@gmail.com>
2025-06-05 20:03:42 +0600

    logo fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,34 +18,36 @@ const Navbar = () => {
 	// ]
 
 	return (
-		<div className="py-3 bg-black text-white ">
+		<div className="py-5 bg-black text-white ">
 			<Container>
 				<div className="flex justify-between items-center">
-					<div>
-						<a href="#"><Logo></Logo></a>
+					<div className="z-40">
+						<a href="#">
+							<Logo></Logo>
+						</a>
 					</div>
 					<div className="flex justify-between items-center">
 						<ul className="flex gap-x-15">
 							<a className="relative group" href="#">
-                <li>Home</li>
-                <div className="absolute -left-3 group-hover:h-full group-hover:w-[2px] ease-in-out  duration-250 top-1/2 transform -translate-y-1/2  h-[2px] w-[0px] bg-white"></div>
-              </a>
+								<li>Home</li>
+								<div className="absolute -left-3 group-hover:h-full group-hover:w-[2px] ease-in-out  duration-250 top-1/2 transform -translate-y-1/2  h-[2px] w-[0px] bg-white"></div>
+							</a>
 							<a className="relative group" href="#">
-                <li>About me</li>
-                <div className="absolute -left-3 group-hover:h-full group-hover:w-[2px] ease-in-out  duration-250 top-1/2 transform -translate-y-1/2  h-[2px] w-[0px] bg-white"></div>
-              </a>
+								<li>About me</li>
+								<div className="absolute -left-3 group-hover:h-full group-hover:w-[2px] ease-in-out  duration-250 top-1/2 transform -translate-y-1/2  h-[2px] w-[0px] bg-white"></div>
+							</a>
 							<a className="relative group" href="#">
-                <li>Skills</li>
-                <div className="absolute -left-3 group-hover:h-full group-hover:w-[2px] ease-in-out  duration-250 top-1/2 transform -translate-y-1/2  h-[2px] w-[0px] bg-white"></div>
-              </a>
+								<li>Skills</li>
+								<div className="absolute -left-3 group-hover:h-full group-hover:w-[2px] ease-in-out  duration-250 top-1/2 transform -translate-y-1/2  h-[2px] w-[0px] bg-white"></div>
+							</a>
 							<a className="relative group" href="#">
-                <li>Portfolio</li>
-                <div className="absolute -left-3 group-hover:h-full group-hover:w-[2px] ease-in-out  duration-250 top-1/2 transform -translate-y-1/2  h-[2px] w-[0px] bg-white"></div>
-              </a>
+								<li>Portfolio</li>
+								<div className="absolute -left-3 group-hover:h-full group-hover:w-[2px] ease-in-out  duration-250 top-1/2 transform -translate-y-1/2  h-[2px] w-[0px] bg-white"></div>
+							</a>
 						</ul>
 						<div>
 							<a href="#">
-								<p className="px-[32px] py-[15px] ml-12 border border-[#2c2c2c] bg-[#1a1a1a] text-white text-lg font-secondary rounded-[30px]   ">
+								<p className="px-[32px] py-[10px] ml-12 border border-[#2c2c2c] bg-[#1a1a1a] text-white text-lg font-secondary rounded-[30px]   ">
 									Contact
 								</p>
 							</a>
