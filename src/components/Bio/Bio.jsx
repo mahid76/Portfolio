ef2903dab2e8d710ea7dae3be1dfca7ea6037e26
Poblaaa <mahidulislamsohan76@gmail.com>
2025-06-05 21:57:18 +0600

    Home Fixed

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -6,8 +6,8 @@ const Bio = () => {
     <section className='py-14 bg-black '>
       <Container>
         <div className='max-w-[772px]'>
-          <h3 className='font-bold text-[30px] text-white tracking-[18.54%] font-primary  '>"Let’s Build Something Great"</h3>
-          <p className='font-secondary text-sm text-white leading-[155.7%] mt-9 '>If you're looking for a developer who cares as much about design as they do about functionality, let's connect. Whether you have a startup idea or need help polishing your existing site, I’m ready to help.</p>
+          <h3 className='font-bold text-[30px]  text-white tracking-[20.54%] font-primary  '>"Let’s Build Something Great"</h3>
+          <p className='font-secondary tracking-wide text-[15px] text-white leading-[155.7%] mt-9 '>If you're looking for a developer who cares as much about design as they do about functionality, let's connect. Whether you have a startup idea or need help polishing your existing site, I’m ready to help.</p>
           <div className="flex  mt-[60px]">
 								<a href="#">
 									<p className="overflow-hidden text-sm font-bold  text-white bg-none rounded-2xl py-3 px-[34px] border-x border-x-white relative group hover:text-black z-10  duration-[400ms] ease-in-out">
