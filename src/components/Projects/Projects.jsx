e0f6511927cfb1af9635453e666c45b873360683
Poblaaa <mahidulislamsohan76@gmail.com>
2025-06-15 03:22:25 +0600

    Projects Responsive

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import { ReactLenis } from "lenis/react";
 import { BiRightTopArrowCircle } from "react-icons/bi";
 import agenc from "../../assets/agenc.jpg";
 import cleaning from "../../assets/cleaning.jpg";
+import portfolioCover from "../../assets/portfolioCover.jpg";
 import innovate from "../../assets/innovate.jpg";
 import Container from "../Layout/Container";
 import Navbar from "../Navbar/Navbar";
@@ -18,7 +19,7 @@ const Projects = () => {
 	const projects = [
 		{
 			name: "Innovate",
-			year: "Mar2025",
+			year: "May2025",
 			align: "right",
 			link: "https://innovate-react-01-git-main-md-mahidul-islams-projects.vercel.app/",
 			code: "https://github.com/mahid76/Innovate_React_01",
@@ -26,7 +27,7 @@ const Projects = () => {
 		},
 		{
 			name: "Agenc",
-			year: "Jan2025",
+			year: "Dec2024",
 			align: "left",
 			link: "https://mahid76.github.io/Agenc_Project02_CIT/",
 			code: "https://github.com/mahid76/Agenc_Project02_CIT",
@@ -34,7 +35,7 @@ const Projects = () => {
 		},
 		{
 			name: "Cleaning",
-			year: "Feb2025",
+			year: "Nov2024",
 			align: "right",
 			link: "https://mahid76.github.io/Cleaning_Project01_CIT/",
 			code: "https://github.com/mahid76/Cleaning_Project01_CIT",
@@ -42,16 +43,16 @@ const Projects = () => {
 		},
 		{
 			name: "Portfolio",
-			year: "Apr2025",
+			year: "Jun2025",
 			align: "left",
-			link: "#",
-			code: "https://github.com/mahid76",
-			image: agenc,
+			link: "https://portfolio-iota-one-bhub1wc80k.vercel.app/",
+			code: "https://github.com/mahid76/Portfolio",
+			image: portfolioCover,
 		},
 	];
 	return (
 		<ReactLenis root>
-			<div className="">
+			<div className="bg-black">
 				<section className="relative">
 					<div
 						className="bg-black absolute top-0 left-0 w-full h-full"
@@ -66,7 +67,7 @@ const Projects = () => {
 					</div>
 					<Navbar></Navbar>
 					<Container>
-						<div className="py-[140px] text-center">
+						<div className="md:py-[140px] py-[50px] text-center">
 							<div className="flex justify-center flex-col">
 								<BlurText
 									text="Projects"
@@ -74,7 +75,7 @@ const Projects = () => {
 									animateBy="words"
 									direction="top"
 									onAnimationComplete={blurAnimationComplete}
-									className="tracking-wider font-bold font-primary leading-20 text-4xl    text-white max-w-[600px] mx-auto"
+									className="tracking-wider font-bold font-primary leading-20 text-3xl md:text-4xl    text-white max-w-[600px] mx-auto"
 								/>
 								<BlurText
 									text="I have worked on a variety of web development projects, ranging from responsive websites for small businesses to
@@ -83,13 +84,13 @@ const Projects = () => {
 									animateBy="words"
 									direction="top"
 									onAnimationComplete={blurAnimationComplete}
-									className="text-[#d5d3d3] tracking-wide font-secondary  text-[18px]  font-medium mt-6 max-w-[600px] mx-auto"
+									className="text-[#d5d3d3] tracking-wide font-secondary  text-sm md:text-[18px]  font-medium md:mt-6 max-w-[600px] mx-auto"
 								/>
 							</div>
-							<div className="all">
+							<div className="">
 								{projects.map((item) => (
 									<div
-										className={`flex w-full mt-20 sm:flex-col-reverse items-center gap-15 ${
+										className={`flex md:flex-row flex-col w-full mt-10 md:mt-20 sm:flex-col-reverse items-center gap-5 md:gap-15 ${
 											item.align === "left"
 												? "md:flex-row"
 												: "md:flex-row-reverse"
@@ -106,11 +107,11 @@ const Projects = () => {
 												transition: { duration: 0.8, delay: 0.5 },
 											}}
 										>
-											<h2 className="text-white font-medium tracking-wider font-primary sm:text-2xl ">
+											<h2 className="text-white font-medium tracking-wider font-primary text-left text-[16px] md:text-2xl ">
 												{item.name}
 											</h2>
 											<p
-												className={`text-white  tracking-wide font-secondary ${
+												className={`text-white text-[14px] md:text-lg tracking-wide font-secondary text-left ${
 													item.align === "left"
 														? "md:text-right"
 														: "md:text-left"
@@ -121,7 +122,7 @@ const Projects = () => {
 											<a
 												target="_blank"
 												href={item.link}
-												className={`text-lg gap-2 font-secondary tracking-wide flex items-center text-cyan-300 opacity-80 hover:opacity-100
+												className={`text-sm md:text-lg gap-2 font-secondary tracking-wide flex items-center text-cyan-300 md:opacity-80 hover:opacity-100
 											 transition-all duration-500 cursor-pointer sm:justify-self-center ${
 													item.align === "left"
 														? "md:justify-self-end"
@@ -134,7 +135,7 @@ const Projects = () => {
 											<a
 												target="_blank"
 												href={item.code}
-												className={`text-lg gap-2 font-secondary tracking-wide flex items-center text-cyan-300  opacity-80 hover:opacity-100
+												className={`text-[14px] md:text-lg gap-2 font-secondary tracking-wide flex items-center text-cyan-300  md:opacity-80 hover:opacity-100
 											 transition-all duration-500 cursor-pointer sm:justify-self-center ${
 													item.align === "left"
 														? "md:justify-self-end"
@@ -158,15 +159,19 @@ const Projects = () => {
 											}}
 											target="_blank"
 											href={item.link}
+											className="  [--card-height:160px] [--card-width:280px]
+  sm:[--card-height:180px] sm:[--card-width:320px]
+  md:[--card-height:200px] md:[--card-width:360px]
+  lg:[--card-height:220px] lg:[--card-width:400px]"
 										>
 											<TiltedCard
 												imageSrc={item.image}
 												altText="projects"
 												captionText="Live"
-												containerHeight="220px"
-												containerWidth="400px"
-												imageHeight="220px"
-												imageWidth="400px"
+												containerHeight="var(--card-height)"
+												containerWidth="var(--card-width)"
+												imageHeight="var(--card-height)"
+												imageWidth="var(--card-width)"
 												rotateAmplitude={12}
 												scaleOnHover={1.1}
 												showMobileWarning={false}
